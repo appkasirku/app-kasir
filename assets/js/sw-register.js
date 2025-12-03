230c4deab9506487da52ca20a3dadeb83fc3508a
appkasirku <app.kasirku@gmail.com>
2025-12-03 21:23:04 +0700

    Update sw-register.js

--- assets/js/sw-register.js
+++ assets/js/sw-register.js
@@ -1,7 +1,7 @@
 async function setServiceWorker() {
 	if (!("serviceWorker" in navigator)) return;
 	try {
-		await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+		await navigator.serviceWorker.register("/sw.js", { scope: "/app-kasir/" });
 		console.log("SW registered");
 	} catch (err) {
 		console.error("SW failed", err);
